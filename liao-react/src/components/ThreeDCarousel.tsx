8c8413cd1bc55504da554d886693d1c0c79a567f
Deivisnan Bispo <liaoufba@gmail.com>
2026-03-15 19:51:23 -0300

    style: implement luxury royalty design with golden borders and lion emblem

--- liao-react/src/components/ThreeDCarousel.tsx
+++ liao-react/src/components/ThreeDCarousel.tsx
@@ -65,7 +65,7 @@ const ThreeDCarousel: React.FC<ThreeDCarouselProps> = ({ partners }) => {
 
                                     {/* Back Face (LIAO Logo) */}
                                     <div className="carousel-card-back">
-                                        <img src="/logo.png" alt="LIAO" className="back-logo" />
+                                        <img src="/luxury-lion.png" alt="LIAO" className="back-logo" />
                                     </div>
                                 </div>
                             </div>
